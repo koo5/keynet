540624ec4162e9556902f34b302923e8546b0fd2
kook <kook@hc595.(none)>
2012-02-24 10:53:23 +0100

    hmm

--- k.php
+++ k.php
@@ -1,9 +1,5 @@
 <?php
 
-/* Create a TCP/IP socket. */
-$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-$result = socket_connect($socket, 'arduino.skynet.666.666.66', '666');
-socket_write($socket, $_GET['key'], 1);
-
+file_put_contents('aaaa', $_GET['key']+$_GET['what']);
 
 ?>
